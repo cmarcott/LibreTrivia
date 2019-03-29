2fa48cf9f3ad9779075d1b5d72ef936ae372fd31
Lamonte Luong <lamonte.luong@gmail.com>
2019-03-28 23:14:28 -0400

    Tasks #6240 #6264 #6265 Resolved

--- app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/ShareActivity.java
+++ app/src/main/java/io/github/trytonvanmeer/libretrivia/activities/ShareActivity.java
@@ -1,30 +1,93 @@
 package io.github.trytonvanmeer.libretrivia.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.IntentFilter;
+import android.content.Context;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.Spinner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import androidx.appcompat.app.AppCompatDelegate;
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import io.github.trytonvanmeer.libretrivia.R;
-import io.github.trytonvanmeer.libretrivia.database.SQLiteDBHelper;
-import io.github.trytonvanmeer.libretrivia.trivia.TriviaCategory;
-import io.github.trytonvanmeer.libretrivia.trivia.TriviaDifficulty;
-import io.github.trytonvanmeer.libretrivia.trivia.TriviaQuery;
-import io.github.trytonvanmeer.libretrivia.util.TypeUtil;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import java.util.Set;
+import android.widget.Toast;
+
 
 public class ShareActivity extends BaseActivity {
+    @BindView(R.id.button_scan_devices)
+    Button btnSearchForDevice;
+    @BindView(R.id.button_enable_discoverable)
+    Button btnEnableDiscoverable;
+
+    private BluetoothAdapter bluetoothAdapter = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share);
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableBtIntent);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(receiver, filter);
+
+        btnEnableDiscoverable = findViewById(R.id.button_enable_discoverable);
+        btnEnableDiscoverable.setOnClickListener(v -> {
+            if (bluetoothAdapter.getScanMode() !=
+                    BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+                startActivity(discoverableIntent);
+            }
+        });
+
+        btnSearchForDevice = findViewById(R.id.button_search_for_device);
+        btnSearchForDevice.setOnClickListener(v -> {
+            Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, 1);
+        });
+
     }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
+    }
+
 }
